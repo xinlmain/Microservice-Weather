fe30462a01ace953067c550527b78910dae91843
xinl <xinlmain@163.com>
2018-09-11 23:38:31 +0800

    '实现eureka自动发现'

--- weather-city-sevice/src/main/java/com/xxx/weather/citysevice/WeatherCitySeviceApplication.java
+++ weather-city-sevice/src/main/java/com/xxx/weather/citysevice/WeatherCitySeviceApplication.java
@@ -2,8 +2,10 @@ package com.xxx.weather.citysevice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class WeatherCitySeviceApplication {
 
     public static void main(String[] args) {
--- weather-report-service/src/main/java/com/xxx/weather/reportservice/WeatherReportServiceApplication.java
+++ weather-report-service/src/main/java/com/xxx/weather/reportservice/WeatherReportServiceApplication.java
@@ -2,8 +2,11 @@ package com.xxx.weather.reportservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WeatherReportServiceApplication {
 
     public static void main(String[] args) {
